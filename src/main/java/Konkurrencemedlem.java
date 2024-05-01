4200325e37760108425155616d0a4f7bb330497c
AndersNystrupJ <anje0008@stud.kea.dk>
2024-05-01 10:24:56 +0200

    fdf

--- src/main/java/Konkurrencemedlem.java
+++ src/main/java/Konkurrencemedlem.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class Konkurrencemedlem extends Medlemmer{
 
-    public Konkurrencemedlem(String navn, int alder, boolean aktivtMedlemskab, boolean juniorMedlemskab) {
+    String træner;
+    String hold = getAlder() < 18 ? "Ungdomshold" : "Seniorhold";
+    ArrayList<SvømmedisciplinOgResultater> svømmedisciplinOgResultater;
+
+
+
+    public Konkurrencemedlem(String navn, int alder, boolean aktivtMedlemskab, boolean juniorMedlemskab, String træner, String hold, ArrayList<SvømmedisciplinOgResultater> svømmedisciplinOgResultater) {
         super (navn, alder, aktivtMedlemskab, juniorMedlemskab);
+        this.træner = træner;
+        this.hold = hold;
+        this.svømmedisciplinOgResultater = svømmedisciplinOgResultater;
     }
 }
---
+++ src/main/java/SvømmedisciplinOgResultater.java
@@ -0,0 +1,30 @@
+public class SvømmedisciplinOgResultater {
+
+    private String svømmedisciplin;
+    private int resultat;
+
+
+
+
+    ////////////////////////
+    // NOGET DATO HER EVT //
+    ////////////////////////
+
+
+
+    public String getSvømmedisciplin(){
+        return svømmedisciplin;
+    }
+
+    public int getResultat(){
+        return resultat;
+    }
+
+    public void setSvømmedisciplin(String svømmedisciplin){
+        this.svømmedisciplin = svømmedisciplin;
+    }
+
+    public void setResultat(int resultat){
+        this.resultat = resultat;
+    }
+}
